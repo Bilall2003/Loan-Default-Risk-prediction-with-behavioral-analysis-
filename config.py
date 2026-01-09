c64261c336012424357a469a8ce0ff25480bbf15
Bilal Ahmed <bilallahmedd685@gmail.com>
2026-01-09 15:13:21 +0000

    add unsupervised.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 from sqlalchemy import create_engine 
 from urllib.parse import quote_plus
 
-DB_USERNAME="your username"
-DB_PASSWORD="your password"
-DB_HOST="your host"
-DB_NAME=" your db_name"
+DB_USERNAME="root"
+DB_PASSWORD="Bilalahmed@1234"
+DB_HOST="localhost"
+DB_NAME="practice2_seperatefile_method"
 
 PASSWORD=quote_plus(DB_PASSWORD)
 ENGINE = create_engine(
-    f"mysql+pymysql://:{PASSWORD}/{DB_NAME}"
+    f"mysql+pymysql://app_user:{PASSWORD}@localhost/{DB_NAME}"
 )
 
-CSV_PATH=r"your file path"
-TABLE_NAME="table name"
+CSV_PATH=r"D:\Bilal folder\internship\task4\credit_risk_dataset.csv"
+TABLE_NAME="data_info"
